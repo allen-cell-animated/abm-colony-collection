758c9bf3490f2f9c4a3dba0725ebe5a681e5fc68
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-03-28 16:58:37 -0400

    Update adding task decorator to init imports (#17)

--- src/abm_colony_collection/__init__.py
+++ src/abm_colony_collection/__init__.py
@@ -1,14 +1,31 @@
 import importlib
-import os
 import sys
 
 from prefect import task
 
-for module_file in os.listdir(os.path.dirname(__file__)):
-    if "__" in module_file or not module_file.endswith(".py"):
-        continue
+from .calculate_centrality_measures import calculate_centrality_measures
+from .calculate_cluster_distances import calculate_cluster_distances
+from .calculate_cluster_sizes import calculate_cluster_sizes
+from .calculate_degree_measures import calculate_degree_measures
+from .calculate_distance_measures import calculate_distance_measures
+from .convert_to_network import convert_to_network
+from .get_depth_map import get_depth_map
+from .get_neighbors_map import get_neighbors_map
+from .make_voxels_array import make_voxels_array
 
-    module_name = module_file.replace(".py", "")
+TASK_MODULES = [
+    calculate_centrality_measures,
+    calculate_cluster_distances,
+    calculate_cluster_sizes,
+    calculate_degree_measures,
+    calculate_distance_measures,
+    convert_to_network,
+    get_depth_map,
+    get_neighbors_map,
+    make_voxels_array,
+]
 
-    module = importlib.import_module(f".{module_name}", package=__name__)
-    setattr(sys.modules[__name__], module_name, task(getattr(module, module_name)))
+for task_module in TASK_MODULES:
+    MODULE_NAME = task_module.__name__
+    module = importlib.import_module(f".{MODULE_NAME}", package=__name__)
+    setattr(sys.modules[__name__], MODULE_NAME, task(getattr(module, MODULE_NAME)))
