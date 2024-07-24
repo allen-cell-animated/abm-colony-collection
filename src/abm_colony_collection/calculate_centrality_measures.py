17313def90ce18a7cd7960752153aed703f57212
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-24 13:46:02 -0400

    Add docstring and unit test for calculate centrality measures task

--- src/abm_colony_collection/calculate_centrality_measures.py
+++ src/abm_colony_collection/calculate_centrality_measures.py
@@ -3,6 +3,28 @@ import pandas as pd
 
 
 def calculate_centrality_measures(network: nx.Graph) -> pd.DataFrame:
+    """
+    Calculate centrality measures for each node in network.
+
+    Measures include:
+
+    - Degree centrality = quantifies how many neighbors a node has
+    - Closeness centrality = quantifies how close a node is to all other nodes
+      in the network
+    - Betweenness centrality = quantifies the extent to which a vertex lies on
+      shortest paths between other vertices
+
+    Parameters
+    ----------
+    network
+        The network object.
+
+    Returns
+    -------
+    :
+        Centrality measures for each node in the network.
+    """
+
     # Calculate different centrality measures for network.
     degree_centralities = nx.degree_centrality(network)
     closeness_centralities = nx.closeness_centrality(network)
